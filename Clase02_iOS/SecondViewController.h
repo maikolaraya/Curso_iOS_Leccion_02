b2dbb505cf34d4d8a30888b5b72eada3dc9e1c5b
Maikol Araya Esquivel <maraya@lacreativeria.com>
2014-02-05 18:39:18 -0600

    First Commit

---
+++ Clase02_iOS/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  Clase02_iOS
+//
+//  Created by Maikol Araya on 1/29/14.
+//  Copyright (c) 2014 La Creativeria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+@property (strong, nonatomic) NSString *contentLabel;
+
+@end
